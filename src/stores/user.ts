2f4a85811396240808a3d720ab56aa67ae16bb36
EdouardRouch <56657928+Doidoire@users.noreply.github.com>
2022-12-23 00:53:47 +0100

    Renommage de la fonction isLoggedIn

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,17 +1,14 @@
 import { defineStore } from "pinia";
-
-const url = "http://" + window.location.hostname;
+import API_URL from '@/apiUrl';
 
 export const useUserStore = defineStore("user", {
   state: () => ({
     status: '',
     user: ''
   }),
-
   actions: {
     async signUp(username: string, password: string, password_verify: string) {
-      const url = "http://" + window.location.hostname + ":8888";
-      const res = await fetch(url + "/api/user/post.php", {
+      const res = await fetch(API_URL + "/api/user/post.php", {
         method: "POST",
         headers: {
           'Content-Type': "application/json; charset=UTF-8",
@@ -30,8 +27,7 @@ export const useUserStore = defineStore("user", {
       return await res.json();
     },
     async logIn(username: string, password: string) {
-      const url = "http://" + window.location.hostname + ":8888";
-      const res = await fetch(url + "/api/user/login.php", {
+      const res = await fetch(API_URL + "/api/user/login.php", {
         method: "POST",
         headers: {
           'Content-Type': "application/json; charset=UTF-8",
@@ -46,18 +42,18 @@ export const useUserStore = defineStore("user", {
       return await res.json()
     },
     async logOut() {
-      const res = await fetch(url + "/api/user/logout.php", { credentials: 'include' });
+      const res = await fetch(API_URL + "/api/user/logout.php", { credentials: 'include' });
 
       if (!res.ok) { throw await res.json(); }
 
       this.$reset();
       return await res.json();
     },
-    async isLoggedIn() {
-      const res = await fetch(url + "/api/user/session_status.php", { credentials: 'include' });
+    async setSessionStatus() {
+      const res = await fetch(API_URL + "/api/user/session_status.php", { credentials: 'include' });
 
       const json = await res.json();
       this.user = await json.records.user;
     }
-  },
+  }
 });
