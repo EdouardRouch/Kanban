8c050e91807e390da724f13d0dd2c7e78d9a6624
EdouardRouch <56657928+Doidoire@users.noreply.github.com>
2022-12-23 00:54:02 +0100

    Configuration de vue-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores/user';
+import Dashboard from '@/components/Dashboard.vue'
+import Kanban from '@/components/kanban/Kanban.vue'
+import App from '@/App.vue'
+import Home from '@/components/Home.vue'
+import Setup from '@/components/Setup.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { name: 'Root', path: '/', redirect: '/dashboard' },
+    { name: 'Home', path: '/home', component: Home },
+    { name: 'Dashboard', path: '/dashboard', component: Dashboard },
+    { name: 'Kanban', path: '/kanban/:id', component: Kanban },
+    { name: 'Setup', path: '/setup', component: Setup },
   ]
 })
 
+const protectedRoutes = [
+  'Dashboard',
+]
+router.beforeEach(async (to, from) => {
+  const userStore = useUserStore();
+
+  if (protectedRoutes.includes(to.name as string)) {
+    await userStore.setSessionStatus();
+    if (userStore.user === '') {
+      return '/home';
+    }
+  }
+})
+
 export default router
+
