0534c5b82ab1973d2f343958ff25e86a8b8f116d
EdouardRouch <56657928+Doidoire@users.noreply.github.com>
2022-12-23 03:03:45 +0100

    Suppression fichier de test

--- public/api/test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    require __DIR__.'/vendor/autoload.php';
-
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-
-    echo $_ENV['S3_BUCKET'];
-    echo $_ENV['SECRET_KEY'];
-?>
